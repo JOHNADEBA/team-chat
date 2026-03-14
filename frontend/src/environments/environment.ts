3ce7147a6ec4fce853ca260918452cac021538c8
John Adeba <adebajohn@gmail.com>
2026-03-14 09:45:16 +0100

    Increase budget limits for CSS

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://team-chat-z7th.onrender.com/api/api',
-  socketUrl: 'https://team-chat-z7th.onrender.com/api',
+  apiUrl: 'https://team-chat-z7th.onrender.com/api',
+  socketUrl: 'https://team-chat-z7th.onrender.com',
 };
-// http://localhost:3001',
