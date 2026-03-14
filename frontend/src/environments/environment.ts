18e16e7f1f58b6011bb4edf84c65d9086a98bf5e
John Adeba <adebajohn@gmail.com>
2026-03-14 11:25:08 +0100

    fix guard on prod

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://team-chat-gamma.vercel.app/api',
-  socketUrl: 'https://team-chat-gamma.vercel.app',
+  apiUrl: 'https://team-chat-z7th.onrender.com/api',
+  socketUrl: 'https://team-chat-z7th.onrender.com',
 };
 // http://localhost:3001',
